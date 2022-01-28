36db95d6e05c470f2f967023dfd14cee331a345d
abinshaji07 <abinshajiphilip2023@cs.ajce.in>
2022-01-28 11:16:20 +0530

    yeah this is first commit

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:ninjacard() ,
+));
+class ninjacard extends StatefulWidget {
+  const ninjacard({Key? key}) : super(key: key);
+
+
+
+  @override
+  State<ninjacard> createState() => _ninjacardState();
+}
+
+class _ninjacardState extends State<ninjacard> {
+
+ int ninjalevel =0;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Ninja id card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+         setState(() {
+          ninjalevel += 1;
+         });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0,30.0 , 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/ninja.jpg'
+
+                ),
+                radius: 40.0,
+
+              ),
+            ),
+
+            Divider(
+              height:60.0 ,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Abin Shaji',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '$ninjalevel',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                    'abin01@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1.0,
+                  ),
+                        ),
+
+              ],
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
